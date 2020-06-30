5e1af6045ed73f2f96d9f492311b4a59e8106f4d
Maxim Mironyuk <serieznyi@gmail.com>
2020-06-30 14:22:48 +0300

    mmironyuk-lesson-1 use google-style-format

--- mironyuk/1/Task1.java
+++ mironyuk/1/Task1.java
@@ -5,40 +5,42 @@ import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Integer.MIN_VALUE;
 
 class Task1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Integer numberOne = null;
-        Integer numberTwo = null;
-        String restriction = String.format("Допустимы числа из диапазона [%s - %s]", MIN_VALUE, MAX_VALUE);
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Integer numberOne = null;
+    Integer numberTwo = null;
+    String restriction =
+        String.format("Допустимы числа из диапазона [%s - %s]", MIN_VALUE, MAX_VALUE);
 
-        while (numberOne == null || numberTwo == null) {
-            try {
-                System.out.printf("Введите первое число. %s:\n", restriction);
-                numberOne = Integer.parseInt(scanner.next());
+    while (numberOne == null || numberTwo == null) {
+      try {
+        System.out.printf("Введите первое число. %s:\n", restriction);
+        numberOne = Integer.parseInt(scanner.next());
 
-                System.out.printf("Введите второе число. %s: \n", restriction);
-                numberTwo = Integer.parseInt(scanner.next());
-            } catch (NumberFormatException e) {
-                System.out.println("Упс! Кажется вы ввели какую-то абракадабру или слишком длинное число. Давай-те повторим ...");
-            }
-        }
-
-        showBigger(numberOne, numberTwo);
-
-        showSum(numberOne, numberTwo);
+        System.out.printf("Введите второе число. %s: \n", restriction);
+        numberTwo = Integer.parseInt(scanner.next());
+      } catch (NumberFormatException e) {
+        System.out.println(
+            "Упс! Кажется вы ввели какую-то абракадабру или слишком длинное число. Давай-те повторим ...");
+      }
     }
 
-    private static void showBigger(Integer numberOne, Integer numberTwo) {
-        if (numberOne > numberTwo) {
-            System.out.printf("Число %s больше %s\n", numberOne, numberTwo);
-        } else {
-            System.out.printf("Число %s меньше %s\n", numberTwo, numberOne);
-        }
-    }
+    showBigger(numberOne, numberTwo);
 
-    private static void showSum(int numberOne, int numberTwo) {
-        BigInteger sum = BigInteger.valueOf(numberOne).add(BigInteger.valueOf(numberTwo));
+    showSum(numberOne, numberTwo);
+  }
 
-        System.out.println("Сумма чисел: " + sum);
+  private static void showBigger(Integer numberOne, Integer numberTwo) {
+    if (numberOne > numberTwo) {
+      System.out.printf("Число %s больше %s\n", numberOne, numberTwo);
+    } else {
+      System.out.printf("Число %s меньше %s\n", numberTwo, numberOne);
     }
+  }
+
+  private static void showSum(int numberOne, int numberTwo) {
+    BigInteger sum = BigInteger.valueOf(numberOne).add(BigInteger.valueOf(numberTwo));
+
+    System.out.println("Сумма чисел: " + sum);
+  }
 }
