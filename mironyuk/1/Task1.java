c2e7e368d7f7c71317ee0ac4d104367899ac501f
Maxim Mironyuk <serieznyi@gmail.com>
2020-06-30 14:17:25 +0300

    mmironyuk-lesson-1 use infinitive loop for read input

--- mironyuk/1/Task1.java
+++ mironyuk/1/Task1.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Integer numberOne;
-        Integer numberTwo;
+        Integer numberOne = null;
+        Integer numberTwo = null;
 
-        System.out.println("Введите первое число: ");
-        numberOne = scanner.nextInt();
+        while (numberOne == null || numberTwo == null) {
+            try {
+                System.out.println("Введите первое число: ");
+                numberOne = Integer.parseInt(scanner.next());
 
-        System.out.println("Введите второе число: ");
-        numberTwo = scanner.nextInt();
+                System.out.println("Введите второе число: ");
+                numberTwo = Integer.parseInt(scanner.next());
+            } catch (NumberFormatException e) {
+                System.out.println("Упс! Кажется вы ввели какую-то абракадабру или слишком длинное число. Давай-те повторим ...");
+            }
+        }
 
         if (numberOne > numberTwo) {
             System.out.printf("Число %s больше %s\n", numberOne, numberTwo);
