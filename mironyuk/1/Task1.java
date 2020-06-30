b9e13d16160b886c88638a558f5bc4b1ba3b51cd
Maxim Mironyuk <serieznyi@gmail.com>
2020-06-30 14:17:28 +0300

    mmironyuk-lesson-1 add restriction message

--- mironyuk/1/Task1.java
+++ mironyuk/1/Task1.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
 class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Integer numberOne = null;
         Integer numberTwo = null;
+        String restriction = String.format("Допустимы числа из диапазона [%s - %s]", MIN_VALUE, MAX_VALUE);
 
         while (numberOne == null || numberTwo == null) {
             try {
-                System.out.println("Введите первое число: ");
+                System.out.printf("Введите первое число. %s:\n", restriction);
                 numberOne = Integer.parseInt(scanner.next());
 
-                System.out.println("Введите второе число: ");
+                System.out.printf("Введите второе число. %s: \n", restriction);
                 numberTwo = Integer.parseInt(scanner.next());
             } catch (NumberFormatException e) {
                 System.out.println("Упс! Кажется вы ввели какую-то абракадабру или слишком длинное число. Давай-те повторим ...");
