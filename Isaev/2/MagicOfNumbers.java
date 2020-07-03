e569100c9a87fa94f15a688ca65d36df5d01f47a
AlexanderIsaev1995 <Alexander.095.Isaev@gmail.com>
2020-07-03 20:59:55 +0300

    Added the part of second homework

---
+++ Isaev/2/MagicOfNumbers.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class MagicOfNumbers {
+
+    public static void main(String[] args) {
+
+        Random MyRandom = new Random();
+
+        int[] randomNumbers = MyRandom.
+
+    }
+
+}
