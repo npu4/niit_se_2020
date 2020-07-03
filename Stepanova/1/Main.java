a8fdf294d7d8148611a43af15cdd491f5ba91f84
LUCY <sss1151@yandex.ru>
2020-07-03 16:46:03 +0300

    ..

---
+++ Stepanova/1/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        Integer a = 5;
+        Integer b = 6;
+
+        if ( a > b ) {
+
+            System.out.println( "Число a больше b" );
+
+
+                    } else {
+            System.out.println( "Число a меньше b" );
+        }
+            System.out.println( a + b  );
+        }
+
+    }
+
+
+
+
