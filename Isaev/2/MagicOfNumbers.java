4439d39aff646fe434a8add8fc3c043c2afbce25
AlexanderIsaev1995 <Alexander.095.Isaev@gmail.com>
2020-07-04 19:26:02 +0300

    Added the part of second homework

--- Isaev/2/MagicOfNumbers.java
+++ Isaev/2/MagicOfNumbers.java
@@ -6,10 +6,20 @@ public class MagicOfNumbers {
 
     public static void main(String[] args) {
 
-        Random MyRandom = new Random();
-
-        int[] randomNumbers = MyRandom.
-
+        Random myRandom = new Random();
+        int[] randomNumbers = myRandom.ints(-100, 100).limit(100).toArray();
+        for (int i : randomNumbers) {
+            if (isMagicNumber(i)) {
+                System.out.println("Число '" + i + "' - магическое!");
+            }
+        }
     }
 
+    static boolean isMagicNumber(int number) {
+        int absoluteNumber = Math.abs(number);
+        return (absoluteNumber / 10 == absoluteNumber % 10);
+    }
 }
+
+
+
