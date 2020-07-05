227b4372ef17a0cafe2e5ff020870512c7e86cba
Oleg Dagestanskiy <dageoleg@mail.ru>
2020-07-05 10:59:42 +0300

    Edited Homework lection2

--- Dagestanskiy/2/src/com/company/AttractionOfOpposites.java
+++ Dagestanskiy/2/src/com/company/AttractionOfOpposites.java
@@ -1,35 +1,31 @@
 package com.company;
+
 import java.util.Random;
+
 import static java.lang.Integer.signum;
 
 public class AttractionOfOpposites {
 
     public static void main(String[] args) {
-        int arraynums[] = new int[10];
-        Random myrandom = new Random();
-        for (int i=0; i<10; i++){
+        int arrayNums[] = new int[10];
+        Random myRandom = new Random();
+        for (int i = 0; i < 10; i++) {
             // записываем в массив случайные целые числа в
             // интервале генератора
-            arraynums[i] = myrandom.nextInt();
-            System.out.printf(" Элемент массива %2d : %15d  %n",i+1,arraynums[i]);
+            arrayNums[i] = myRandom.nextInt();
+            System.out.printf(" Элемент массива %2d : %15d  %n", i + 1, arrayNums[i]);
         }
-        for (int i=0; i<9; i++){
-            if(ArrCompare(arraynums[i],arraynums[i+1])){
+        for (int i = 0; i < 9; i++) {
+            if (arrCompare(arrayNums[i], arrayNums[i + 1])) {
                 System.out.printf("Числа с противоположными знаками обнаружены: %15d  %15d %n",
-                        arraynums[i], arraynums[i+1]);
+                        arrayNums[i], arrayNums[i + 1]);
             }
         }
     }
 
-     static boolean ArrCompare(int a , int b) {
-        if (Integer.signum(a)*Integer.signum(b)  < 0 ){
-            return true;
-        } else {
-            return false;
-        }
+    static boolean arrCompare(int a, int b) {
+         return Integer.signum(a) * Integer.signum(b) < 0 ;
     }
-
-
 
 
 }
