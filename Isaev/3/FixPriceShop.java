d4c2f176035d0b65b02de469fb4dac7ee00eb64c
AlexanderIsaev1995 <Alexander.095.Isaev@gmail.com>
2020-07-08 19:32:44 +0300

    Part of third homework

--- Isaev/3/FixPriceShop.java
+++ Isaev/3/FixPriceShop.java
@@ -1,11 +1,11 @@
-import java.lang.reflect.Array;
+
 import java.util.Arrays;
-import java.util.Random;
+
 
 
 public class FixPriceShop {
 
-    String[] items = {"coffee","tea","chocolate","mint","bread","pens","socks","cat's food"};
+    String[] items = {"coffee", "tea", "chocolate", "mint", "bread", "pens", "socks", "cat's food"};
     int itemPrice = 100;
     int happyHour;
 
@@ -18,45 +18,49 @@ public class FixPriceShop {
         this.happyHour = happyHour;
     }
 
-    int checkItemPrice(String item, int hour){
-       int findItems = Arrays.asList(items).indexOf(item);
-       if (findItems == -1){
-           return -1;
-       } else if (hour == happyHour){
-           return  itemPrice / 2;
-       } else {
-           return itemPrice;
-       }
+    int checkItemPrice(String item, int hour) {
+        int findItems = Arrays.asList(items).indexOf(item);
+        if (findItems == -1) {
+            return -1;
+        } else if (hour == happyHour) {
+            return itemPrice / 2;
+        } else {
+            return itemPrice;
+        }
     }
 
-    String[] getItems(){
+    String[] getItems() {
         return items;
     }
 
-    void buyItem(String item,int hour) {
+    void buyItem(String item, int hour) {
         System.out.println("Goods " + item + " sold at a price " + checkItemPrice(item, hour));
-        int indexOfItem = Arrays.asList(items).indexOf(item);
-        items[indexOfItem] = "bought";
-        String[] newItems = new String[items.length - 1];
-        System.arraycopy(items,0,newItems,0,);
+        String[] remainingItem = new String[items.length - 1];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == item) {
+                for (; i < items.length - 1; i++) {
+                    items[i] = items[i + 1];
+                }
+                for (i = 0; i < items.length - 1; i++) {
+                    remainingItem[i] = items[i];
+                }
+                items = remainingItem;
+            }
+        }
     }
 
     public static void main(String[] args) {
 
-
         FixPriceShop shop = new FixPriceShop();
-        System.out.println(shop.checkItemPrice("coffee",14));
+        System.out.println(shop.checkItemPrice("coffee", 14));
         String[] currentItems = shop.getItems();
-        for (String i : currentItems){
+        for (String i : currentItems) {
             System.out.println(i);
         }
-        shop.buyItem("coffee",10);
-        for (String i : currentItems){
+        shop.buyItem("coffee", 10);
+        String[] newCurrentItems = shop.getItems();
+        for (String i : newCurrentItems) {
             System.out.println(i);
         }
-
-
-
     }
-
 }
