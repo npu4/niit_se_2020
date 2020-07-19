80afa82652605b4edae74e89cc70b7d6a6518469
Maxim Mironyuk <serieznyi@gmail.com>
2020-07-19 23:17:18 +0300

    mmironyuk-lesson-4 remove monster regeneration

--- mironyuk/4/src/org/serieznyi/FightOfWizards/character/Monster.java
+++ mironyuk/4/src/org/serieznyi/FightOfWizards/character/Monster.java
@@ -6,16 +6,10 @@ import org.serieznyi.FightOfWizards.Scene;
  *  - Монстры восстанавливают немного здоровья после своего действия
  */
 final public class Monster extends Character {
-    private static final double REGENERATION_FACTOR = 0.1;
-    private final int REGENERATION_DEGRADATION_SIZE;
     private final int damageSize;
-    private int regenerationSize;
 
     public Monster(String name, int health, int damage) {
         super(name, health);
-
-        regenerationSize = (int) (health * REGENERATION_FACTOR);
-        REGENERATION_DEGRADATION_SIZE = (int) (regenerationSize * REGENERATION_FACTOR);
 
         damageSize = damage;
     }
@@ -27,23 +21,5 @@ final public class Monster extends Character {
         opponent.decreaseHealth(damageSize);
 
         System.out.printf("Монстр \"%s\" атакует \"%s\" на %s единиц урона\n", name, opponent.name, damageSize);
-
-        regenerate();
-    }
-
-    private void regenerate() {
-        if (regenerationSize <= 0) {
-            return;
-        }
-
-        int healthBefore = getHealth();
-        int newHealth = increaseHealth(regenerationSize);
-
-        if (healthBefore != newHealth) {
-            System.out.printf("\tМонстр \"%s\" восстановил свое здоровье на \"%s\". Теперь у него \"%s\" здоровья\n", getName(), regenerationSize, newHealth);
-
-            regenerationSize -= REGENERATION_DEGRADATION_SIZE;
-            System.out.printf("\tРегенерация у монстра \"%s\" ослабла\n", name);
-        }
     }
 }
