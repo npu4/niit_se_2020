98efb9297dc6cad859c0cb98031f41cacd05244d
Maxim Mironyuk <serieznyi@gmail.com>
2020-07-26 22:50:20 +0300

    mmironyuk-lesson-6 format code

--- mironyuk/6/src/org/serieznyi/tuple/Main.java
+++
@@ -1,6 +0,0 @@
-package org.serieznyi.tuple;
-
-public class Main {
-  public static void main(String[] args) {
-  }
-}
--- mironyuk/6/src/org/serieznyi/tuple/Triple.java
+++ mironyuk/6/src/org/serieznyi/tuple/Triple.java
@@ -3,43 +3,40 @@ package org.serieznyi.tuple;
 import java.util.Objects;
 
 public class Triple<V1, V2, V3> {
-    private final V1 valueOne;
-    private final V2 valueTwo;
-    private final V3 valueThree;
+  private final V1 valueOne;
+  private final V2 valueTwo;
+  private final V3 valueThree;
 
-    public Triple(V1 valueOne, V2 valueTwo, V3 valueThree)
-    {
-        this.valueOne = valueOne;
-        this.valueTwo = valueTwo;
-        this.valueThree = valueThree;
-    }
+  public Triple(V1 valueOne, V2 valueTwo, V3 valueThree) {
+    this.valueOne = valueOne;
+    this.valueTwo = valueTwo;
+    this.valueThree = valueThree;
+  }
 
-    public V1 getFirst()
-    {
-        return valueOne;
-    }
+  public V1 getFirst() {
+    return valueOne;
+  }
 
-    public V2 getSecond()
-    {
-        return valueTwo;
-    }
+  public V2 getSecond() {
+    return valueTwo;
+  }
 
-    public V3 getValueThree() {
-        return valueThree;
-    }
+  public V3 getValueThree() {
+    return valueThree;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Triple<?, ?, ?> triple = (Triple<?, ?, ?>) o;
-        return Objects.equals(valueOne, triple.valueOne) &&
-                Objects.equals(valueTwo, triple.valueTwo) &&
-                Objects.equals(valueThree, triple.valueThree);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Triple<?, ?, ?> triple = (Triple<?, ?, ?>) o;
+    return Objects.equals(valueOne, triple.valueOne)
+        && Objects.equals(valueTwo, triple.valueTwo)
+        && Objects.equals(valueThree, triple.valueThree);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(valueOne, valueTwo, valueThree);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(valueOne, valueTwo, valueThree);
+  }
 }
