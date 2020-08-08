2755dc11a5bc631d0b282752d2b1d4155567f056
Maxim Mironyuk <serieznyi@gmail.com>
2020-08-08 15:30:00 +0300

    mmironyuk-lesson-8 add test for serializer

---
+++ mironyuk/8/tests/data/classesForSerialization/PersonWithPrimitiveFieldsWithoutOverriddenNames.java
@@ -0,0 +1,54 @@
+package data.classesForSerialization;
+
+import org.serieznyi.serialization.serializer.annotation.Serialize;
+
+import java.util.Objects;
+
+@Serialize()
+public final class PersonWithPrimitiveFieldsWithoutOverriddenNames {
+  public String firstName = "Max";
+
+  public int age = 12;
+
+  public long phone = 789318064471L;
+
+  public short height = 172;
+
+  public float rating = 12.5F;
+
+  public boolean hasChildes = false;
+
+  public char favoriteChar = 'A';
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    PersonWithPrimitiveFieldsWithoutOverriddenNames that = (PersonWithPrimitiveFieldsWithoutOverriddenNames) o;
+    return age == that.age &&
+            phone == that.phone &&
+            height == that.height &&
+            Float.compare(that.rating, rating) == 0 &&
+            hasChildes == that.hasChildes &&
+            favoriteChar == that.favoriteChar &&
+            firstName.equals(that.firstName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, age, phone, height, rating, hasChildes, favoriteChar);
+  }
+
+  @Override
+  public String toString() {
+    return "PersonWithPrimitiveFieldsWithoutOverriddenNames{" +
+            "firstName='" + firstName + '\'' +
+            ", age=" + age +
+            ", phone=" + phone +
+            ", height=" + height +
+            ", rating=" + rating +
+            ", hasChildes=" + hasChildes +
+            ", favoriteChar=" + favoriteChar +
+            '}';
+  }
+}
---
+++ mironyuk/8/tests/org/serieznyi/serialization/serializer/SerializerTest.java
@@ -0,0 +1,50 @@
+package org.serieznyi.serialization.serializer;
+
+import data.Pair;
+import data.classesForSerialization.PersonWithLinkOnPerson;
+import data.classesForSerialization.PersonWithPrimitiveFieldsWithOverriddenNames;
+import data.classesForSerialization.PersonWithPrimitiveFieldsWithoutOverriddenNames;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.serieznyi.serialization.serializer.exception.FormatNotSupportedException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+final class SerializerTest {
+    private final Serializer serializer = new Serializer();
+
+    private static List<Pair<Object, Serializer.Format>> objects() {
+        List<Object> classesForSerialization = Arrays.asList(
+            new PersonWithPrimitiveFieldsWithoutOverriddenNames(),
+            new PersonWithPrimitiveFieldsWithOverriddenNames(),
+            new PersonWithLinkOnPerson(new PersonWithPrimitiveFieldsWithoutOverriddenNames())
+        );
+
+        List<Pair<Object, Serializer.Format>> data = new ArrayList<>();
+
+        for (Serializer.Format format : Serializer.Format.values()) {
+            for (Object o : classesForSerialization) {
+                data.add(new Pair<>(o, format));
+            }
+        }
+
+        return data;
+    }
+
+    @ParameterizedTest
+    @MethodSource("objects")
+    void testWhatSerializingAndDeserializingIsSuccessful(Pair<Object, Serializer.Format> pair) throws FormatNotSupportedException {
+        Object object = pair.getFirst();
+        Serializer.Format format = pair.getSecond();
+
+        String serializeResult = serializer.serialize(object, format);
+
+        Object deserializedObject = serializer.deserialize(serializeResult, object.getClass(), format);
+
+        assertEquals(object, deserializedObject, "Десериализованный объект равен сериализованному");
+    }
+}
