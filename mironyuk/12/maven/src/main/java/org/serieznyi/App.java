5a4441b914d3623491bd5e062dd850e46275d4ab
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2020-08-14 23:42:37 +0300

    mmironyuk-lesson-12 init maven

---
+++ mironyuk/12/maven/src/main/java/org/serieznyi/App.java
@@ -0,0 +1,13 @@
+package org.serieznyi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
