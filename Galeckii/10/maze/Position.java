a8e762815fd89eb97fd0144785ce8b6b9b8bb584
gringokir <gringokir@gmail.com>
2020-08-14 09:42:05 +0300

    Galeckii 10 homework

---
+++ Galeckii/10/maze/Position.java
@@ -0,0 +1,54 @@
+package maze;
+
+import java.util.Objects;
+
+public class Position {
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Position(Position position) {
+        this(position.getX(), position.getY());
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x &&
+                y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
