5bffef0fe7e5a6cc1012c24541c755ff813e304a
Maxim Mironyuk (serieznyi) <serieznyi@gmail.com>
2020-08-25 08:54:35 +0300

    mmironyuk-lesson-10 save

--- mironyuk/10/src/org/serieznyi/maze/MazeGenerator.java
+++ mironyuk/10/src/org/serieznyi/maze/MazeGenerator.java
@@ -2,35 +2,35 @@ package org.serieznyi.maze;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MazeGenerator {
     public int[][] generate(int width, int height)
     {
-        Node[][] maze = new Node[width][height];
+        Node[][] maze = makeMaze(width, height);
 
-        for (int y = 0; y < maze.length; y++) {
-            for (int x = 0; x < maze[y].length; x++) {
+        int[][] intMaze = mazeToIntArray(maze);
 
-                maze[x][y] = new Node(x, y);
+        return addWallsAround(intMaze);
+    }
 
-                List<Node> neig = new ArrayList<>();
+    private int[][] addWallsAround(int[][] maze) {
+        int[][] mazeWithWalls = new int[maze.length + 1][maze[0].length + 1];
 
-                if (indexInBound(maze, x, y - 1)) {
-                    neig.add(new Node(x, y - 1));
-                }
-
-                if (indexInBound(maze, x - 1, y)) {
-                    neig.add(new Node(x - 1, y));
-                }
-
-                if (neig.size() > 0) {
-                    maze[x][y].connect = neig.get(ThreadLocalRandom.current().nextInt(0, neig.size()));
+        for (int y = 0; y < mazeWithWalls.length; y++) {
+            for (int x = 0; x < mazeWithWalls[y].length; x++) {
+                if (x != 0 && y != 0 && x != mazeWithWalls.length - 1 && y != mazeWithWalls[y].length - 1) {
+                    mazeWithWalls[x][y] = maze[x - 1][y - 1];
                 }
             }
         }
 
-        int[][] data = new int[width * 2 - 1][height * 2 - 1];
+        return mazeWithWalls;
+    }
+
+    private int[][] mazeToIntArray(Node[][] maze) {
+        int[][] intMaze = new int[maze.length * 2 - 1][maze[0].length * 2 - 1];
 
         for (int y = 0; y < maze.length; y++) {
             for (int x = 0; x < maze[y].length; x++) {
@@ -40,33 +40,67 @@ public class MazeGenerator {
                 int newX = node.x * 2;
                 int newY = node.y * 2;
 
-                data[newX][newY] = 1;
+                intMaze[newX][newY] = 1;
 
                 if (connected != null) {
                     int newCoX = connected.x * 2;
                     int newCoY = connected.y * 2;
 
-                    data[newCoX][newCoY] = 1;
+                    intMaze[newCoX][newCoY] = 1;
 
                     int xa = (newX + newCoX) / 2;
                     int ya = (newY + newCoY) / 2;
 
-                    data[xa][ya] = 1;
+                    intMaze[xa][ya] = 1;
                 }
             }
         }
 
-        int[][] data2 = new int[data.length + 1][data[0].length + 1];
+        return intMaze;
+    }
 
-        for (int y = 0; y < data2.length; y++) {
-            for (int x = 0; x < data2[y].length; x++) {
-                if (x != 0 && y != 0 && x != data2.length - 1 && y != data2[y].length - 1) {
-                    data2[x][y] = data[x - 1][y - 1];
-                }
+    private Node[][] makeMaze(int width, int height) {
+        Node[][] maze = new Node[width][height];
+
+        for (int y = 0; y < maze.length; y++) {
+            for (int x = 0; x < maze[y].length; x++) {
+
+                Node node = maze[x][y] = new Node(x, y);
+
+                getAnyNeighbor(maze, node).ifPresent(n -> node.connect = n);
             }
         }
 
-        return data2;
+        return maze;
+    }
+
+    private Optional<Node> getAnyNeighbor(Node[][] maze, Node node) {
+        List<Node> neighbors = getNeighborsFor(maze, node);
+
+        if (neighbors.size() == 0) {
+            return Optional.empty();
+        }
+
+        int random = ThreadLocalRandom.current().nextInt(0, neighbors.size());
+
+        return Optional.of(neighbors.get(random));
+    }
+
+    private List<Node> getNeighborsFor(Node[][] maze, Node node) {
+        List<Node> neighbors = new ArrayList<>();
+
+        int x = node.x;
+        int y = node.y;
+
+        if (indexInBound(maze, x, y - 1)) {
+            neighbors.add(new Node(x, y - 1));
+        }
+
+        if (indexInBound(maze, x - 1, y)) {
+            neighbors.add(new Node(x - 1, y));
+        }
+
+        return neighbors;
     }
 
     public static boolean indexInBound(Node[][] arrays, int x, int y){
