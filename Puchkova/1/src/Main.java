90e319cdcb8f22715944b23d7cbf34dc24bce80c
npu4 <nataha1910@mail.ru>
2020-09-07 12:42:17 +0300

    Homework 1

---
+++ Puchkova/1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer a = Integer.parseInt(args[0]);
+        Integer b = Integer.parseInt(args[1]);
+
+        if (a > b) System.out.printf("Число %s больше %s", a, b);
+        else System.out.printf("Число %s меньше %s", a, b);
+    }
+}
